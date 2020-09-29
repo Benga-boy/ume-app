f9680511bd085c7ba2ed8bc19b4511199fdef209
Benga <benga.olasebikan@gmail.com>
2020-09-29 12:12:31 +0100

    ready for first deployment

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const authRoute = require('./routes/api/auth')
 const postRoute = require('./routes/api/posts')
 const profileRoute = require('./routes/api/profile')
 const messageRoute = require('./routes/api/messages')
+const path = require('path')
 
 const app = express()
 
@@ -15,7 +16,7 @@ connectDB()
 
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => res.send('API RUNNING!!!'))
+
 
 
 // * Define the routes
@@ -24,6 +25,16 @@ app.use('/api/auth', authRoute)
 app.use('/api/profile', profileRoute)
 app.use('/api/posts', postRoute)
 app.use('/api/message', messageRoute)
+
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 8000
 
